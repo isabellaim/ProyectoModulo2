4a542018f94d0cba98f4aebf1406677903138c71
Isa <iimartin@espol.edu.ec>
2025-06-22 18:53:15 -0500

    Final 4

--- 🏠_Inicio.py
+++ 🏠_Inicio.py
@@ -80,5 +80,11 @@ integrantes = [
 ]
 
 for col, integrante in zip(cols, integrantes):
+
     with col:
-        st.image(integrante["foto"], width=150, caption=f"{integrante['nombre']}", use_container_width=True)
+        st.write("Ruta imagen:", integrante["foto"])
+        import os
+        if not os.path.exists(integrante["foto"]):
+            st.error(f"No se encuentra la imagen: {integrante['foto']}")
+        else:
+            st.image(integrante["foto"], width=150, caption=f"{integrante['nombre']}", use_container_width=True)
